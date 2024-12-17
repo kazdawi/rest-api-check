adf7f3a52e5abaa8868ac8c89e84a8205b9c3c26
aziz <kazdawiaziz@gmail.com>
2024-12-18 00:11:44 +0100

    first one

---
+++ server.js
@@ -0,0 +1,26 @@
+// require express
+const express = require("express");
+//require dotenv
+require("dotenv").config();
+
+//instance app
+const app = express();
+//middelware  bodyparser
+app.use(express.json())
+//connect to DB
+const connectDB = require("./config/connectDB");
+connectDB();
+
+
+app.use('/api/contact', require('./routes/contact'))
+
+//PORT
+const PORT = process.env.PORT || 2500;
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error("Error starting the server:", err);
+    } else {
+        console.log(`The server is running on PORT: http://localhost:${PORT}`);
+    }
+});
